86f34e01eb2d0df30860fab5db53dbca3bf03982
Tsuzuki <c0a2108861@edu.teu.ac.jp>
2023-04-06 00:33:42 +0900

    新しい形態のHPに移行

---
+++ main3.js
@@ -0,0 +1,70 @@
+$(function(){
+	$("#gi0").click(function(){
+		$(".pop_img").fadeOut();
+		$(".pop_txt").fadeOut();
+		$(".pi0").fadeIn();
+		$(".pt0").fadeIn();
+		return false;
+	});
+});
+$(function(){
+	$("#gi1").click(function(){
+		$(".pop_img").fadeOut();
+		$(".pop_txt").fadeOut();
+		$(".pi1").fadeIn();
+		$(".pt1").fadeIn();
+		return false;
+	});
+});
+$(function(){
+	$("#gi2").click(function(){
+		$(".pop_img").fadeOut();
+		$(".pop_txt").fadeOut();
+		$(".pi2").fadeIn();		
+		$(".pt2").fadeIn();
+		return false;
+	});
+});
+$(function(){
+	$("#gi3").click(function(){
+		$(".pop_img").fadeOut();
+		$(".pop_txt").fadeOut();
+		$(".pi3").fadeIn();		
+		$(".pt3").fadeIn();
+		return false;
+	});
+});
+$(function(){
+	$("#gi4").click(function(){
+		$(".pop_img").fadeOut();
+		$(".pop_txt").fadeOut();
+		$(".pi4").fadeIn();		
+		$(".pt4").fadeIn();
+		return false;
+	});
+});
+$(function(){
+	$("#gi5").click(function(){
+		$(".pop_img").fadeOut();
+		$(".pop_txt").fadeOut();
+		$(".pi5").fadeIn();		
+		$(".pt5").fadeIn();
+		return false;
+	});
+});
+$(function(){
+	$("#gi6").click(function(){
+		$(".pop_img").fadeOut();
+		$(".pop_txt").fadeOut();
+		$(".pi6").fadeIn();		
+		$(".pt6").fadeIn();
+		return false;
+	});
+});
+$(function(){
+	$("body").click(function(){
+		$(".pop_img").fadeOut();
+		$(".pop_txt").fadeOut();
+		return false;
+	});
+});
